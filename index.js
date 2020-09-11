87948fdedee52eb2c5b325a284de08ba729ea77a
Sanmu3 <mujahidmuslim0@gmail.com>
2020-09-12 06:39:35 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+var nilaiSantri = [50, 90, 65, 67, 89, 80, 50, 97, 84];
+
+var total = 0;
+var bnyNilai = nilaiSantri.length;
+
+for(i = 0; i < bnyNilai; i++){
+    console.log(i)
+
+    total = nilaiSantri[i]
+}
+
+document.write(total/bnyNilai)
